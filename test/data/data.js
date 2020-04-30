01305926d088fc6d7966eaae35a468b0b4346ad3
Betsy Vidal <ing.betsyvies@gmail.com>
2020-04-30 16:37:21 -0500

    add unit teste

---
+++ test/data/data.js
@@ -0,0 +1,391 @@
+const arrLinksFileOptVal = [
+  {
+    href: 'https://nodejs.org/api/fs.html',
+    text: 'fs',
+    file: '/Users/betsyvidal/Desktop/md-links/test/readmes/example.md',
+    status: 200,
+    message: 'ok',
+  },
+  {
+    href: 'https://nodejs.org/api/path.html',
+    text: 'path',
+    file: '/Users/betsyvidal/Desktop/md-links/test/readmes/example.md',
+    status: 200,
+    message: 'ok',
+  },
+  {
+    href: 'https://docs.npmjs.com/files/package.json',
+    text: 'package.json',
+    file: '/Users/betsyvidal/Desktop/md-links/test/readmes/example.md',
+    status: 200,
+    message: 'ok',
+  },
+  {
+    href: 'https://nodejs.org/docs/latest-v0.10.x/api/modules.html',
+    text: 'CommonJS',
+    file: '/Users/betsyvidal/Desktop/md-links/test/readmes/example.md',
+    status: 200,
+    message: 'ok',
+  },
+  {
+    href: 'https://www.npmjs.com/',
+    text: 'npm',
+    file: '/Users/betsyvidal/Desktop/md-links/test/readmes/example.md',
+    status: 200,
+    message: 'ok',
+  },
+  {
+    href: 'https://docs.npmjs.com/misc/scripts',
+    text: 'npm-scripts',
+    file: '/Users/betsyvidal/Desktop/md-links/test/readmes/example.md',
+    status: 200,
+    message: 'ok',
+  },
+  {
+    href:
+      'https://nodejs.org/api/http.html#http_http_get_options_callback',
+    text: 'http.get',
+    file: '/Users/betsyvidal/Desktop/md-links/test/readmes/example.md',
+    status: 200,
+    message: 'ok',
+  },
+  {
+    href: 'https://desarrolloweb.com/articulos/buffer-en-nodejs.html',
+    text: 'articulBuffer',
+    file: '/Users/betsyvidal/Desktop/md-links/test/readmes/example.md',
+    status: 200,
+    message: 'ok',
+  },
+  {
+    href: 'https://es.wikipedia.org/wiki/Truncamiento',
+    text: 'wiki-trunca',
+    file: '/Users/betsyvidal/Desktop/md-links/test/readmes/example.md',
+    status: 200,
+    message: 'ok',
+  },
+  {
+    href:
+      'https://jarroba.com/busqueda-de-patrones-expresiones-regulares/',
+    text: 'Regular Expressions',
+    file: '/Users/betsyvidal/Desktop/md-links/test/readmes/example.md',
+    status: 200,
+    message: 'ok',
+  },
+  {
+    href:
+      'https://franl2p.wordpress.com/2015/04/01/primeros-pasos-con-node-js-y-express/',
+    text: 'JS',
+    file: '/Users/betsyvidal/Desktop/md-links/test/readmes/example.md',
+    status: 200,
+    message: 'ok',
+  },
+  {
+    href:
+      'https://gitlab.com/betsyvies/node-curso/blob/master/3-seccion/app.js',
+    text: 'Instalar módulos',
+    file: '/Users/betsyvidal/Desktop/md-links/test/readmes/example.md',
+    status: 302,
+    message: 'fail',
+  },
+  {
+    href:
+      'http://tecnops.es/testing-en-javascript-con-jest-parte-1-de-2/',
+    text: 'TestTing',
+    file: '/Users/betsyvidal/Desktop/md-links/test/readmes/example.md',
+    status: 403,
+    message: 'fail',
+  },
+  {
+    href:
+      'http://tecnops.es/testing-en-javascript-con-jest-parte-1-de-2/',
+    text: 'TestTing-1',
+    file: '/Users/betsyvidal/Desktop/md-links/test/readmes/example.md',
+    status: 403,
+    message: 'fail',
+  },
+];
+
+const arrLinksDirOptVal = [
+  {
+    href: 'https://nodejs.org/api/fs.html',
+    text: 'fs',
+    file: '/Users/betsyvidal/Desktop/md-links/test/readmes/example.md',
+    status: 200,
+    message: 'ok',
+  },
+  {
+    href: 'https://nodejs.org/api/path.html',
+    text: 'path',
+    file: '/Users/betsyvidal/Desktop/md-links/test/readmes/example.md',
+    status: 200,
+    message: 'ok',
+  },
+  {
+    href: 'https://docs.npmjs.com/files/package.json',
+    text: 'package.json',
+    file: '/Users/betsyvidal/Desktop/md-links/test/readmes/example.md',
+    status: 200,
+    message: 'ok',
+  },
+  {
+    href: 'https://nodejs.org/docs/latest-v0.10.x/api/modules.html',
+    text: 'CommonJS',
+    file: '/Users/betsyvidal/Desktop/md-links/test/readmes/example.md',
+    status: 200,
+    message: 'ok',
+  },
+  {
+    href: 'https://www.npmjs.com/',
+    text: 'npm',
+    file: '/Users/betsyvidal/Desktop/md-links/test/readmes/example.md',
+    status: 200,
+    message: 'ok',
+  },
+  {
+    href: 'https://docs.npmjs.com/misc/scripts',
+    text: 'npm-scripts',
+    file: '/Users/betsyvidal/Desktop/md-links/test/readmes/example.md',
+    status: 200,
+    message: 'ok',
+  },
+  {
+    href:
+      'https://nodejs.org/api/http.html#http_http_get_options_callback',
+    text: 'http.get',
+    file: '/Users/betsyvidal/Desktop/md-links/test/readmes/example.md',
+    status: 200,
+    message: 'ok',
+  },
+  {
+    href: 'https://desarrolloweb.com/articulos/buffer-en-nodejs.html',
+    text: 'articulBuffer',
+    file: '/Users/betsyvidal/Desktop/md-links/test/readmes/example.md',
+    status: 200,
+    message: 'ok',
+  },
+  {
+    href: 'https://es.wikipedia.org/wiki/Truncamiento',
+    text: 'wiki-trunca',
+    file: '/Users/betsyvidal/Desktop/md-links/test/readmes/example.md',
+    status: 200,
+    message: 'ok',
+  },
+  {
+    href:
+      'https://jarroba.com/busqueda-de-patrones-expresiones-regulares/',
+    text: 'Regular Expressions',
+    file: '/Users/betsyvidal/Desktop/md-links/test/readmes/example.md',
+    status: 200,
+    message: 'ok',
+  },
+  {
+    href:
+      'https://franl2p.wordpress.com/2015/04/01/primeros-pasos-con-node-js-y-express/',
+    text: 'JS',
+    file: '/Users/betsyvidal/Desktop/md-links/test/readmes/example.md',
+    status: 200,
+    message: 'ok',
+  },
+  {
+    href:
+      'https://gitlab.com/betsyvies/node-curso/blob/master/3-seccion/app.js',
+    text: 'Instalar módulos',
+    file: '/Users/betsyvidal/Desktop/md-links/test/readmes/example.md',
+    status: 302,
+    message: 'fail',
+  },
+  {
+    href:
+      'http://tecnops.es/testing-en-javascript-con-jest-parte-1-de-2/',
+    text: 'TestTing',
+    file: '/Users/betsyvidal/Desktop/md-links/test/readmes/example.md',
+    status: 403,
+    message: 'fail',
+  },
+  {
+    href:
+      'http://tecnops.es/testing-en-javascript-con-jest-parte-1-de-2/',
+    text: 'TestTing-1',
+    file: '/Users/betsyvidal/Desktop/md-links/test/readmes/example.md',
+    status: 403,
+    message: 'fail',
+  },
+  {
+    href: 'https://docs.npmjs.com/misc/scripts',
+    text: 'npm-scripts',
+    file:
+      '/Users/betsyvidal/Desktop/md-links/test/readmes/other/example2.md',
+    status: 200,
+    message: 'ok',
+  },
+];
+
+const arrLinksFile = [
+  {
+    href: 'https://nodejs.org/api/fs.html',
+    text: 'fs',
+    file: '/Users/betsyvidal/Desktop/md-links/test/readmes/example.md',
+  },
+  {
+    href: 'https://nodejs.org/api/path.html',
+    text: 'path',
+    file: '/Users/betsyvidal/Desktop/md-links/test/readmes/example.md',
+  },
+  {
+    href: 'https://docs.npmjs.com/files/package.json',
+    text: 'package.json',
+    file: '/Users/betsyvidal/Desktop/md-links/test/readmes/example.md',
+  },
+  {
+    href: 'https://nodejs.org/docs/latest-v0.10.x/api/modules.html',
+    text: 'CommonJS',
+    file: '/Users/betsyvidal/Desktop/md-links/test/readmes/example.md',
+  },
+  {
+    href: 'https://www.npmjs.com/',
+    text: 'npm',
+    file: '/Users/betsyvidal/Desktop/md-links/test/readmes/example.md',
+  },
+  {
+    href: 'https://docs.npmjs.com/misc/scripts',
+    text: 'npm-scripts',
+    file: '/Users/betsyvidal/Desktop/md-links/test/readmes/example.md',
+  },
+  {
+    href:
+      'https://nodejs.org/api/http.html#http_http_get_options_callback',
+    text: 'http.get',
+    file: '/Users/betsyvidal/Desktop/md-links/test/readmes/example.md',
+  },
+  {
+    href: 'https://desarrolloweb.com/articulos/buffer-en-nodejs.html',
+    text: 'articulBuffer',
+    file: '/Users/betsyvidal/Desktop/md-links/test/readmes/example.md',
+  },
+  {
+    href: 'https://es.wikipedia.org/wiki/Truncamiento',
+    text: 'wiki-trunca',
+    file: '/Users/betsyvidal/Desktop/md-links/test/readmes/example.md',
+  },
+  {
+    href:
+      'https://jarroba.com/busqueda-de-patrones-expresiones-regulares/',
+    text: 'Regular Expressions',
+    file: '/Users/betsyvidal/Desktop/md-links/test/readmes/example.md',
+  },
+  {
+    href:
+      'https://franl2p.wordpress.com/2015/04/01/primeros-pasos-con-node-js-y-express/',
+    text: 'JS',
+    file: '/Users/betsyvidal/Desktop/md-links/test/readmes/example.md',
+  },
+  {
+    href:
+      'https://gitlab.com/betsyvies/node-curso/blob/master/3-seccion/app.js',
+    text: 'Instalar módulos',
+    file: '/Users/betsyvidal/Desktop/md-links/test/readmes/example.md',
+  },
+  {
+    href:
+      'http://tecnops.es/testing-en-javascript-con-jest-parte-1-de-2/',
+    text: 'TestTing',
+    file: '/Users/betsyvidal/Desktop/md-links/test/readmes/example.md',
+  },
+  {
+    href:
+      'http://tecnops.es/testing-en-javascript-con-jest-parte-1-de-2/',
+    text: 'TestTing-1',
+    file: '/Users/betsyvidal/Desktop/md-links/test/readmes/example.md',
+  },
+];
+
+const arrLinksDir = [
+  {
+    href: 'https://nodejs.org/api/fs.html',
+    text: 'fs',
+    file: '/Users/betsyvidal/Desktop/md-links/test/readmes/example.md',
+  },
+  {
+    href: 'https://nodejs.org/api/path.html',
+    text: 'path',
+    file: '/Users/betsyvidal/Desktop/md-links/test/readmes/example.md',
+  },
+  {
+    href: 'https://docs.npmjs.com/files/package.json',
+    text: 'package.json',
+    file: '/Users/betsyvidal/Desktop/md-links/test/readmes/example.md',
+  },
+  {
+    href: 'https://nodejs.org/docs/latest-v0.10.x/api/modules.html',
+    text: 'CommonJS',
+    file: '/Users/betsyvidal/Desktop/md-links/test/readmes/example.md',
+  },
+  {
+    href: 'https://www.npmjs.com/',
+    text: 'npm',
+    file: '/Users/betsyvidal/Desktop/md-links/test/readmes/example.md',
+  },
+  {
+    href: 'https://docs.npmjs.com/misc/scripts',
+    text: 'npm-scripts',
+    file: '/Users/betsyvidal/Desktop/md-links/test/readmes/example.md',
+  },
+  {
+    href:
+      'https://nodejs.org/api/http.html#http_http_get_options_callback',
+    text: 'http.get',
+    file: '/Users/betsyvidal/Desktop/md-links/test/readmes/example.md',
+  },
+  {
+    href: 'https://desarrolloweb.com/articulos/buffer-en-nodejs.html',
+    text: 'articulBuffer',
+    file: '/Users/betsyvidal/Desktop/md-links/test/readmes/example.md',
+  },
+  {
+    href: 'https://es.wikipedia.org/wiki/Truncamiento',
+    text: 'wiki-trunca',
+    file: '/Users/betsyvidal/Desktop/md-links/test/readmes/example.md',
+  },
+  {
+    href:
+      'https://jarroba.com/busqueda-de-patrones-expresiones-regulares/',
+    text: 'Regular Expressions',
+    file: '/Users/betsyvidal/Desktop/md-links/test/readmes/example.md',
+  },
+  {
+    href:
+      'https://franl2p.wordpress.com/2015/04/01/primeros-pasos-con-node-js-y-express/',
+    text: 'JS',
+    file: '/Users/betsyvidal/Desktop/md-links/test/readmes/example.md',
+  },
+  {
+    href:
+      'https://gitlab.com/betsyvies/node-curso/blob/master/3-seccion/app.js',
+    text: 'Instalar módulos',
+    file: '/Users/betsyvidal/Desktop/md-links/test/readmes/example.md',
+  },
+  {
+    href:
+      'http://tecnops.es/testing-en-javascript-con-jest-parte-1-de-2/',
+    text: 'TestTing',
+    file: '/Users/betsyvidal/Desktop/md-links/test/readmes/example.md',
+  },
+  {
+    href:
+      'http://tecnops.es/testing-en-javascript-con-jest-parte-1-de-2/',
+    text: 'TestTing-1',
+    file: '/Users/betsyvidal/Desktop/md-links/test/readmes/example.md',
+  },
+  {
+    href: 'https://docs.npmjs.com/misc/scripts',
+    text: 'npm-scripts',
+    file: '/Users/betsyvidal/Desktop/md-links/test/readmes/other/example2.md',
+  },
+];
+
+module.exports = {
+  arrLinksFile,
+  arrLinksDir,
+  arrLinksDirOptVal,
+  arrLinksFileOptVal,
+};
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,23 +1,54 @@
-/*const mdLinks = require('../lib/index');
-
+const mdLinks = require('../lib/index');
+const {
+  arrLinksDir,
+  arrLinksFile,
+  arrLinksDirOptVal,
+  arrLinksFileOptVal,
+} = require('./data/data');
 
 describe('mdLinks', () => {
-
-  it('should return a set of objects', () => {
-    mdLinks("./some/example.md")
-    .then(links => {
-      expect(typeof links[0]).toBe('object')
-    })
-    .catch(console.error);
+  test('the path is not validate', () => {
+    mdLinks('./readme')
+      .catch(e => expect(e).toMatch('La ruta no es valida'));
   });
 
-  it('should return a objects', () => {
-    mdLinks("./some/example.md")
-    .then(links => {
-      expect(typeof links[0]).toBe('object')
-    })
-    .catch(console.error);
+  test('the path is not file .md', () => {
+    mdLinks('./readmes/otherFiles.txt')
+      .catch(e => expect(e).toMatch('No se encontro archivo .md'));
   });
 
+  test('should return a set of objects of file', () => {
+    mdLinks('./readmes/example.md')
+      .then((links) => {
+        expect(links).toEqual(arrLinksFile);
+      });
+  });
 
-});*/
+  test('should return a set of obj of dir', () => {
+    mdLinks('./readmes')
+      .then((links) => {
+        expect(links).toEqual(arrLinksDir);
+      });
+  });
+
+  test('should return a set empty', () => {
+    mdLinks('./readmes/example1.md')
+      .then((links) => {
+        expect(links).toEqual([]);
+      });
+  });
+
+  test('should return a set of objects of file and opt-val', () => {
+    mdLinks('./readmes/example.md')
+      .then((links) => {
+        expect(links).toEqual(arrLinksFileOptVal);
+      });
+  });
+
+  test('should return a set of obj of dir and opt-val', () => {
+    mdLinks('./readmes')
+      .then((links) => {
+        expect(links).toEqual(arrLinksDirOptVal);
+      });
+  });
+});
